40c5c939e69124624e20cb37f6eed65c9bb9c44f
Elkopo <42452055+Elkopo@users.noreply.github.com>
2019-08-22 22:14:59 +0930

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,3 +1,15 @@
+/* This file is an example for SDEV350 */
+/* Drop and Create statements */
+DROP TABLE testtab;
+CREATE TABLE testtab (
+    serial INT PRIMARY KEY,
+    first_name VARCHAR(32),
+    last_name VARCHAR(32),
+    narrLine VARCHAR(255)
+);
+/* Insert statements */ 
+INSERT INTO testtab VALUES (1, 'Greg', 'Smith', 'This guy is awesome');
+INSERT INTO testtab VALUES (2, 'New', 'Guy', 'This guy is shady');
 INSERT INTO testtab VALUES (3, 'Some', 'Guy', 'Not cool');
-
+/* Select from test table */
 SELECT * FROM testtab;
